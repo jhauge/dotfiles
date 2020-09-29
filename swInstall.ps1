f679c2599040826a2c8d2bc936a9468e5b2f6313
Jesper Hauge <jesper@iqit.dk>
2020-09-29 12:38:25 +0200

    Some updates to software install

--- swInstall.ps1
+++ swInstall.ps1
@@ -4,17 +4,26 @@ if (-not($env:ChocolateyInstall)) {
     exit
 }
 
-choco install -Y 7zip.install
-choco install -Y azure-data-studio
+# Dev setup
+choco install -Y microsoft-windows-terminal
 choco install -Y vscode.install
+choco install -Y visualstudio2019professional
+choco install -Y netfx-4.8-devpack
+choco install -Y dotnetcore-sdk
+choco install -Y visualstudio2019-workload-azure # VS workload for Azure
+choco install -Y visualstudio2019-workload-netweb # VS workload for Web
+choco install -Y visualstudio2019-workload-netcoretools # VS workload for .net core
+choco install -Y microsoftazurestorageexplorer
+choco install -Y azure-data-studio
+choco install -Y nvm
+choco install -Y postman
+choco install -Y wsl-ubuntu-1804
+choco install -Y vcredist140 # used in node to compile node-sass
+
+# Other tools
+choco install -Y 7zip.install
 choco install -Y dashlane
 choco install -Y FiraCode
 choco install -Y googlechrome
 choco install -Y Firefox
-choco install -Y microsoftazurestorageexplorer
-choco install -Y microsoft-windows-terminal
-choco install -Y nvm
-choco install -Y postman
-choco install -Y wsl-ubuntu-1804
-choco install -Y visualstudio2019professional
 choco install -Y spotify
