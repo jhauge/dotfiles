2efbc7c63a8c4328bf6e428c3bbcc504173e5bc7
Jesper Hauge <jesper@iqit.dk>
2020-11-23 10:22:00 +0100

    Updated profile location and removed comments

--- ConfigFiles/Microsoft.PowerShell_profile.ps1
+++ ConfigFiles/Microsoft.PowerShell_profile.ps1
@@ -11,7 +11,4 @@ Import-Module posh-git
 Import-Module oh-my-posh
 Set-Theme jhauge
 
-# Azure Powershell - disabled due to slow loading time
-# Import-Module Az
-
 $env:path += ";C:\Program Files\Microsoft SQL Server\140\DAC\bin;C:\Program Files\Git\usr\bin\;C:\Program Files (x86)\Microsoft SDKs\Azure\AzCopy\;~\AppData\Local\gmaster\bin\"
--- paths.ps1
+++ paths.ps1
@@ -2,5 +2,5 @@ $docsFolder = [environment]::getfolderpath("mydocuments");
 $wintermSettingsPath = "$env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json";
 $vscodeSettingsPath = "$env:APPDATA\Code\User\settings.json";
 $gitconfigPath = "~\.gitconfig"
-$pwshProfilePath = "$PROFILE"
+$pwshProfilePath = "$PROFILE.CurrentUserAllHosts"
 $ohmyposhProfilePath = "$docsfolder\PowerShell\PoshThemes\jhauge.psm1"
