d4cc271737efaf464f47f641c116d9699f5651ae
Jesper Hauge <jesper@iqit.dk>
2021-04-23 10:40:42 +0200

    Added terminal-icons to startup

--- ConfigFiles/profile.ps1
+++ ConfigFiles/profile.ps1
@@ -3,13 +3,15 @@ Push-Location ~\MyHelpers
 Import-Module .\MyHelpers.psd1
 Pop-Location
 
+# Folder icons
+Import-Module -Name Terminal-Icons
+
 # Z directory browsing
 Import-Module z
 
 # posh-git
-Import-Module posh-git
 Import-Module oh-my-posh
-Set-Theme jhauge
+Set-PoshPrompt C:\Users\jespe\.ohmyposh.jhauge.json
 
 # Aliases
 Set-Alias -Name selcontext -Value Select-AzContext
